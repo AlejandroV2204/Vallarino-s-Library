5375c2a1e0e630a89c5273502ae2cc5ff1251e87
AlejandroV2204 <91919753+AlejandroV2204@users.noreply.github.com>
2024-11-21 18:51:14 -0500

    Inicialiazación del proyecto

---
+++ client/app/Components/navbar/page.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+export default function Navbar(){
+    return(
+        <>
+        <ul>
+            <li>
+                <link rel="stylesheet" href="/login" >LogIn</link>
+            </li>
+            <li>
+                <link rel="stylesheet" href="/home" >Home</link>
+            </li>
+        </ul>
+        </>
+    )
+}
---
+++ client/app/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="w-full container flex items-center mt-5 py-5">
+            <p>All rights reserved.</p>
+        </footer>
+    );
+}
